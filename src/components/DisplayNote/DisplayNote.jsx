95d4652f7d45f88172aea61b591adaaffb5659d2
prashikjaware <prashikjaware123@gmail.com>
2021-05-21 19:00:12 +0530

    [Prashik]Added: Added Grid and Paper Material

--- src/components/DisplayNote/DisplayNote.jsx
+++ src/components/DisplayNote/DisplayNote.jsx
@@ -3,54 +3,54 @@ import './DisplayNote.css';
 import Userservice from '../../services/userservice';
 
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
 
 const axios_service = new Userservice();
 
-export default class DisplayNote extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            Title: '',
-            Note: '',
-            Reminder: '',
-            Color: '',
-            Iimage: '',
-            Collaborator: '',
-            IsPin: false,
-            IsArchive: false,
-            IsTrash: false,
-            toOpenNote: false,
-            TitleError: false,
-            NoteError: false
-        }
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-    Get = () => {
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(2);
 
-        axios_service.DisplayNote().then((result) => {
-            console.log(result);
+  const [Title, setTitle] = React.useState();
+  const [Message, setMessage] = React.useState();
+  const [Reminder, setReminder] = React.useState();
+  const [Color, setColor] = React.useState();
+  const [Image, setImage] = React.useState();
+  const [Collaborator, setCollaborator] = React.useState();
+  const [IsPin, setIsPin] = React.useState();
+  const [IsArchive, setIsArchive] = React.useState();
+  const [IsTrash, setIsTrash] = React.useState(2);
 
-        }).catch(() => {
-            console.log();
-        })
-    }
+  const classes = useStyles();
 
+  return (
 
-    render() {
-        return (
-        <>
-            {
-                this.state.Title !== ''
-                ?
-                <div className="Paper">
-                <Paper elevation={0} ></Paper>
-                </div>
-                :
-
-                <></>
-            }
-        </>
-        )
-    }
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={spacing}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      
+  );
 }
